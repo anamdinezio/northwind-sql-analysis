868ba12676b32de5ebfef9ce68055c0e2ff13728
Ana Di Nezio <anamdinezio@gmail.com>
2026-05-06 17:41:58 -0400

    Update 05_yoy_growth.sql

--- 05_yoy_growth.sql
+++ 05_yoy_growth.sql
@@ -9,3 +9,30 @@
 
 WITH YearlyRevenue AS (
     SELECT
+        YEAR(o.OrderDate) AS [Year],
+        SUM(od.UnitPrice * od.Quantity) AS Revenue
+
+    FROM Orders o
+        INNER JOIN [Order Details] od ON o.OrderID = od.OrderID
+
+    GROUP BY
+        YEAR(o.OrderDate)
+)
+
+SELECT
+    [Year],
+    FORMAT(Revenue, 'N2') AS YearlyRevenue,
+
+    -- Previous year revenue
+    FORMAT(LAG(Revenue) OVER (ORDER BY [Year]), 'N2') AS PrevYearRevenue,
+
+    -- Absolute growth
+    FORMAT(Revenue - LAG(Revenue) OVER (ORDER BY [Year]), 'N2') AS YoYDifference,
+
+    -- Growth percentage
+    FORMAT(
+        (Revenue / NULLIF(LAG(Revenue) OVER (ORDER BY [Year]), 0)) - 1, 'P1') AS YoYGrowth
+
+FROM YearlyRevenue
+
+ORDER BY [Year]
